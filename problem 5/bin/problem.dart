ccaa8b931d62eef27b772b0057312ecc15d020e2
mariam hussien <marim.hossen166@gmail.com>
2026-03-13 07:25:04 +0200

    Add files via upload

---
+++ problem 5/bin/problem.dart
@@ -0,0 +1,35 @@
+class Client {
+
+  String? name;
+  String? phone;
+
+  void showClient() {
+    print("Client name: $name");
+    print("Phone: $phone");
+  }
+
+}
+
+class Campaign {
+
+  String? campaignName;
+  double? budget;
+
+  void showCampaign() {
+    print("Campaign: $campaignName");
+    print("Budget: $budget");
+  }
+
+}
+
+class Employee {
+
+  String? name;
+  String? job;
+
+  void showEmployee() {
+    print("Employee: $name");
+    print("Job: $job");
+  }
+
+}
